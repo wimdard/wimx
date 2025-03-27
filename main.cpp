f59f5622f301b131e954a592d5926829fad3c9c6
wimdard <lololog999@gmail.com>
2025-03-27 10:17:38 +0100

    +main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+void showIntro() {
+
+    system("echo '\n"
+           "▄▄▌ ▐ ▄▌▪  • ▌ ▄ ·. ▐▄• ▄ \n"
+           "██· █▌▐███ ·██ ▐███▪ █▌█▌▪\n"
+           "██▪▐█▐▐▌▐█·▐█ ▌▐▌▐█· ·██· \n"
+           "▐█▌██▐█▌▐█▌██ ██▌▐█▌▪▐█·█▌\n"
+           " ▀▀▀▀ ▀▪▀▀▀▀▀  █▪▀▀▀•▀▀ ▀▀' | lolcat");
+    system("echo '\n"
+     "[1]  Option 1         [2]  Option 2          [3]  Option 3         [4]  Option 4\n"
+     "[5]  Option 5         [6]  Option 6          [7]  Option 7         [8]  Option 8\n"
+     "[9]  Option 9         [10] Option 10         [11] Option 11        [12] Option 12\n"
+     "[13] Option 13        [14] Option 14         [15] Option 15        [16] Setting' | lolcat");
+}
+        int main() {
+        showIntro();
+
+        }
